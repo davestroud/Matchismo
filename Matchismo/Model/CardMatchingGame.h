8938eb064c9f57c8c9b0122ba752d0213e53c467
David Stroud <david@greymatter-ag.com>
2013-11-05 18:12:09 -0500

    added cardmatchinggame class

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by DAVID STROUD on 11/5/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by DAVID STROUD on 11/5/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
