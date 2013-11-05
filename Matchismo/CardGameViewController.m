457300064d6c121b2d1cbb38e0e76fa4ec189046
David Stroud <david@greymatter-ag.com>
2013-11-05 16:26:21 -0500

    built playing card deck class

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,20 +7,28 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
 
-- (void)setFlipCount:(int)flipCount
+- (Deck *)deck
 {
-    _flipCount = flipCount;
-    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-    NSLog(@"flipCount changed to %d", self.flipCount);
+    if(!_deck)_deck = [self createDeck];
+    return _deck;
 }
+
+- (Deck *)createDeck
+{
+    return[[PlayingCardDeck alloc]init];
+}
+
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
@@ -34,6 +42,13 @@
         [sender setTitle:@"A♣" forState:UIControlStateNormal];
     }
     self.flipCount++;
+}
+
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
 }
 
 @end
---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by DAVID STROUD on 11/5/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
