4b56572ccda74168fd6ac152c76fa6f8e6823f09
David Stroud <david@greymatter-ag.com>
2013-11-05 13:03:10 -0500

    synthesized suit

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -10,4 +10,35 @@
 
 @implementation PlayingCard
 
+- (NSString *) contents
+{
+    NSArray *rankStrings = @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
+    return [rankStrings [self.rank] stringByAppendingString:self.suit];
+}
+
+@synthesize suit = _suit; //because we provide setter and getter
+
+
+- (void) setSuit:(NSString *)suit
+{
+    if  ([@[@"❤️", @"♦️", @"💔", @"👄"]containsObject:suit])    {
+        _suit = suit;
+    }
+}
+
+
+
+
+
+
+
+- (NSString *) suit;
+{
+    return _suit ? _suit :@"?";
+}
+
+
+
+
+
 @end
