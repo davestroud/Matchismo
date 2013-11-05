4e0897e193f8e376419cb2206504800da4a165d4
David Stroud <david@greymatter-ag.com>
2013-11-05 12:35:33 -0500

    added deck class

---
+++ Matchismo/Model/PlayingCard.h
@@ -0,0 +1,15 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by DAVID STROUD on 11/5/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) NSUInteger *rank;
+@end
---
+++ Matchismo/Model/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by DAVID STROUD on 11/5/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
