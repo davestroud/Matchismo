91c4d9d0b7c9295c047ad9a137965aabfad0d81c
David Stroud <david@greymatter-ag.com>
2013-11-05 15:51:48 -0500

    finished deck class

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -15,24 +15,36 @@
 
 @implementation Deck
 
-- (void)addCard:(Card *)card atTop:(BOOL)atTop
+- (NSMutableArray *)cards
 {
-  
-    
-    
-    
-    
+    if (!_cards)_cards = [[NSMutableArray alloc]init];
+    return  _cards;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop {
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+- (void)addCard:(Card *)card {
+    [self addCard:card atTop:NO];
 }
 
 
-
-- (void)addCard:(Card *)card
+- (Card *)drawRandomCard
 {
-    [self.addCard:card atTop:NO];
+    Card *randomCard = nil;
+    
+    if ([self.cards count])
+    {
+    unsigned index = arc4random() % [self.cards count];
+    randomCard = self.cards[index];
+    [self.cards removeObjectAtIndex:index];
+    }
+    return randomCard;
 }
-
-
-- (Card *)drawRandomCard { }
 
 
 
